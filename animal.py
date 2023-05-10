853400cd17f81292fcde28ae6833d0d0a5bc909c
tongzhouu <zhoudong@stu.sau.edu.cn>
2023-05-10 10:17:52 +0800

    add a cat function

--- animal.py
+++ animal.py
@@ -1,9 +1,14 @@
 import sys
 
+def cat():
+    print('Meow!')
+
 def default():
     print('hello')
 
 def main():
+    if sys.argv[1] == 'cat':
+        cat()
     default()
 
 if __name__ == '__main__':
